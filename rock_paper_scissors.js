914b4d65e02049c9255499374b15ae519aeadb9f
Shireen Kheradpey <shkherad@bu.edu>
2016-05-14 19:36:57 -0400

    rock paper scissors javascript
    Rock, paper, scissors game. javascript demo

---
+++ rock_paper_scissors.js
@@ -0,0 +1,70 @@
+console.log("anything");
+var botScore=0,
+	playerScore=0;
+
+document.getElementById("rock").onclick=playerThrowsRock;
+document.getElementById("paper").onclick=playerThrowsPaper;
+document.getElementById("scissors").onclick=playerThrowsScissors;
+
+
+function playerThrowsRock(){
+	var playersWeapon = "rock";
+	var botsWeapon = getRandomWeapon();
+	checkWhoWon(botsWeapon,"rock");
+}
+
+function playerThrowsScissors(){
+	var playersWeapon = "scissors";
+	var botsWeapon = getRandomWeapon();
+	checkWhoWon(botsWeapon,"scissors");
+
+}
+function playerThrowsPaper(){
+	var playersWeapon = "paper";
+	var botsWeapon = getRandomWeapon();
+	checkWhoWon(botsWeapon,"paper");
+
+}
+function getRandomWeapon(){
+	var randomNumber=Math.random();
+	var botsWeapon="rock";
+	if(randomNumber<.33){
+		botsWeapon="scissors";
+	}
+	else if(randomNumber<.6666){
+		botsWeapon="paper";
+	}
+	
+	return botsWeapon;
+
+}
+function checkWhoWon(botsWeapon,playersWeapon){
+	console.log(playersWeapon)
+	if(botsWeapon==playersWeapon){
+		displayCompleteMessage("There was tie");
+	}
+	else if(
+		(botsWeapon=="scissors" && playersWeapon=="paper") ||
+		(botsWeapon=="paper" && playersWeapon=="rock") ||
+		(botsWeapon=="rock" && playersWeapon=="scissors")
+		){
+		increaseBotScore();
+	}
+	else{
+		increasePlayerScore();
+	}
+}
+function increaseBotScore(){
+	botScore+=1;
+	document.getElementById("computerScore").innerHTML=botScore;
+	displayCompleteMessage("Sorry, you're a loser");
+}
+function increasePlayerScore(){
+	playerScore+=1;
+	document.getElementById("humanScore").innerHTML=playerScore;
+	displayCompleteMessage("Yay, you're a winner!");
+
+}
+function displayCompleteMessage(msg){
+	document.getElementById("status").innerHTML=msg;
+}
